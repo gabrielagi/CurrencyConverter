f88c47f7bebb2bf6e9bb5e3e193683cea879e71b
Gabriela Iriart <gabrielairiart.gi@gmail.com>
2023-10-27 20:34:50 -0300

    feat: Implement menu links for external redirection

--- App/src/screens/Options.js
+++ App/src/screens/Options.js
@@ -1,59 +1,55 @@
 import React from "react";
-import {
-  StyleSheet,
-  TouchableOpacity,
-  Text,
-  View,
-  SafeAreaView,
-} from "react-native";
-
+import { SafeAreaView, ScrollView, StyleSheet, Linking } from "react-native";
 import { Entypo } from "@expo/vector-icons";
-
 import { globalStyles } from "../../globalAndroidStyles.js";
 
 import colors from "../constants/colors";
+import { RowItem, RowSeparator } from "../components/RowItem";
 
 const styles = StyleSheet.create({
-  row: {
-    paddingHorizontal: 20,
-    paddingVertical: 16,
-    justifyContent: "space-between",
-    alignItems: "center",
-    flexDirection: "row",
-    backgroundColor: colors.white,
-  },
-  title: {
-    color: colors.text,
-    fontSize: 16,
-  },
-  separator: {
-    backgroundColor: colors.line,
-    height: StyleSheet.hairlineWidth,
-    marginLeft: 20,
+  container: {
+    // backgroundColor: "#A0E548",
+    flex: 1,
   },
 });
 
+const openLink = (url) =>
+  Linking.openURL(url).catch(() =>
+    Alert.alert("Sorry, something went wrong.", "Please try again later.")
+  );
+
 export default () => {
   return (
-    <SafeAreaView style={globalStyles.androidMargin}>
-      <TouchableOpacity onPress={() => alert("todo!")} style={styles.row}>
-        <Text style={styles.title}>Themes</Text>
-        <Entypo name="chevron-right" size={20} color={colors.blue} />
-      </TouchableOpacity>
+    <SafeAreaView style={[globalStyles.androidMargin, styles.container]}>
+      <ScrollView>
+        <RowItem
+          title="Themes"
+          onPress={() => alert("todo!")}
+          rightIcon={
+            <Entypo name="chevron-right" size={20} color={colors.blue} />
+          }
+        />
 
-      <View style={styles.separator} />
+        <RowSeparator />
 
-      <TouchableOpacity onPress={() => alert("todo!")} style={styles.row}>
-        <Text style={styles.title}>React Native Basics</Text>
-        <Entypo name="export" size={20} color={colors.blue} />
-      </TouchableOpacity>
+        <RowItem
+          title="React Native Basics"
+          onPress={() =>
+            openLink(
+              "https://learn.reactnativeschool.com/p/react-native-basics-build-a-currency-converter"
+            )
+          }
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+        />
 
-      <View style={styles.separator} />
+        <RowSeparator />
 
-      <TouchableOpacity onPress={() => alert("todo!")} style={styles.row}>
-        <Text style={styles.title}>React Native by Example</Text>
-        <Entypo name="export" size={20} color={colors.blue} />
-      </TouchableOpacity>
+        <RowItem
+          title="React Native by Example"
+          onPress={() => openLink("https://reactnativebyexample.com")}
+          rightIcon={<Entypo name="export" size={20} color={colors.blue} />}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 };
