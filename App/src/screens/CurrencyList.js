583c47353d2f5bbc12f5d1f1c9f191383b4ea1a4
Gabriela Iriart <gabrielairiart.gi@gmail.com>
2023-10-30 19:02:45 -0300

    feat: Designate the Currently Selected Currency

--- App/src/screens/CurrencyList.js
+++ App/src/screens/CurrencyList.js
@@ -1,13 +1,37 @@
 import React from "react";
-import { StatusBar, FlatList, View, StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  StatusBar,
+  FlatList,
+  View,
+  StyleSheet,
+  SafeAreaView,
+} from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context"; // Importa useSafeAreaInsets
+import { Entypo } from "@expo/vector-icons";
+
 import currencies from "../data/currencies.json";
 import { RowItem, RowSeparator } from "../components/RowItem";
 import colors from "../constants/colors";
 
 import { globalStyles } from "../../globalAndroidStyles";
 
-export default ({ navigation }) => {
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.blue,
+  },
+});
+
+export default ({ navigation, route = {} }) => {
+  const insets = useSafeAreaInsets(); // Utiliza useSafeAreaInsets para obtener los valores del área segura
+
+  const params = route.params || {};
+  const { activeCurrency } = params;
+
   return (
     <SafeAreaView
       style={StyleSheet.compose(
@@ -19,17 +43,29 @@ export default ({ navigation }) => {
       <FlatList
         data={currencies}
         renderItem={({ item }) => {
+          const selected = activeCurrency === item;
+
           return (
             <RowItem
               title={item}
               onPress={() => {
                 navigation.pop();
               }}
+              rightIcon={
+                selected && (
+                  <View style={styles.icon}>
+                    <Entypo name="check" size={20} color={colors.white} />
+                  </View>
+                )
+              }
             />
           );
         }}
         keyExtractor={(item) => item}
         ItemSeparatorComponent={() => <RowSeparator />}
+        ListFooterComponent={() => (
+          <View style={{ paddingBottom: insets.bottom }} />
+        )}
       />
     </SafeAreaView>
   );
